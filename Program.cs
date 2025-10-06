08819a0b17658a465f0d277323e62d06cc5ba740
Gabriel <gabrielgoxhaj2013@hotmail.it>
2025-10-06 12:09:33 +0200

    Initialized project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LavoratoriEx_10062025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
